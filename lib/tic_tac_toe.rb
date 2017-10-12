45e04c8bbec7184f1825b3fdd5920e6823ef3f72
Zahra Khan <zarssskhan@gmail.com>
2017-10-12 07:11:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,10 +5,13 @@ def initialize
 end
 
 WIN_COMBINATIONS = [
-[0, 1, 2], [3, 4, 5], 
-[6, 7, 8], [0, 3, 6], 
+[0, 1, 2], [3, 4, 5],
+[6, 7, 8], [0, 3, 6],
 [1, 4, 7], [2, 5, 8],
 [0, 4, 8], [2, 4, 6]
 ]
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]}" 
 
 end
