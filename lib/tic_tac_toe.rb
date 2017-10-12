2811b5a82a1bbcc8d717b1d856a614be9f6f6609
Zahra Khan <zarssskhan@gmail.com>
2017-10-12 07:22:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,7 +12,7 @@ class TicTacToe
   ]
 
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
