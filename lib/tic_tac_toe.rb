d1216e560cee9e2f382d377d754cd597635112b4
Zahra Khan <zarssskhan@gmail.com>
2017-10-12 07:12:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,6 +12,10 @@ WIN_COMBINATIONS = [
 ]
 
 def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]}" 
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 
 end
